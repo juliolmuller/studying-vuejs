c0e3ea62a66c103d7bcafe57a46413dbf781fbc9
juliolmuller <juliolmuller@outlook.com>
2023-04-12 00:08:50 -0300

    fix: fix deploy base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/studying-vuejs-crud',
+  base: '/studying-vuejs',
   build: {
     outDir: 'docs',
   },
